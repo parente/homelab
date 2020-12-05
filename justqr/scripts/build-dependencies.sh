707e501550f87df2dd22c752543e118289da7bdb
Peter Parente <parente@gmail.com>
2020-12-05 17:27:43 -0500

    Experiment with multiarch, caching

---
+++ justqr/scripts/build-dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+if [[ "$TARGETARCH" == "arm/v7" ]]; then
+    RUST_TARGET=armv7-unknown-${TARGETOS}-gnueabihf
+elif [[ "$TARGETARCH" == "amd64" ]]; then
+    RUST_TARGET=x86_64-unknown-${TARGETOS}-gnu
+else 
+    echo "Unknown arch: $TARGETARCH"
+    exit 1
+fi
+
+cargo build --release
