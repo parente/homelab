55b8387d4fe04cee90d74311d83f26d53bd22675
Peter Parente <parente@gmail.com>
2020-12-05 23:38:32 -0500

    Fix arm compile

--- justqr/scripts/install-application.sh
+++ justqr/scripts/install-application.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [[ "$TARGETARCH" == "arm/v7" ]]; then
+if [[ "${TARGETARCH}/${TARGETVARIANT}" == "arm/v7" ]]; then
     RUST_TARGET=armv7-unknown-${TARGETOS}-gnueabihf
 elif [[ "$TARGETARCH" == "amd64" ]]; then
     RUST_TARGET=x86_64-unknown-${TARGETOS}-gnu
--- justqr/scripts/install-toolchain.sh
+++ justqr/scripts/install-toolchain.sh
@@ -3,12 +3,12 @@ set -e
 
 apt update
 
-if [[ "$TARGETARCH" == "arm/v7" ]]; then
-    apt install gcc-arm-linux-gnueabihf
+if [[ "${TARGETARCH}/${TARGETVARIANT}" == "arm/v7" ]]; then
+    apt install -y gcc-arm-linux-gnueabihf
     RUST_TARGET=armv7-unknown-${TARGETOS}-gnueabihf
 elif [[ "$TARGETARCH" == "amd64" ]]; then
     RUST_TARGET=x86_64-unknown-${TARGETOS}-gnu
-else 
+else
     echo "Unknown arch: $TARGETARCH"
     exit 1
 fi
